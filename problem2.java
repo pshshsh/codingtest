44a272b9ef951e37b4b6fb172e27b40164d80c1e
park seong ho <hgg0305@daum.net>
2024-12-26 15:32:17 +0900

    problem2

---
+++ problem2.java
@@ -0,0 +1,11 @@
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+class Solution {
+    public int solution(int num1, int num2) {
+        
+    if (num1 < 0 || num1 > 100 || num2 < 0 || num2 > 100) {
+            throw new IllegalArgumentException("Input 0 and 100.");
+        }
+       return num1*num2;
+    }
+}
