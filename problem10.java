9519f1480e5e56a6db1b43802a21a1fb767c2124
park seong ho <hgg0305@daum.net>
2024-12-30 11:20:15 +0900

    문제10

---
+++ problem10.java
@@ -0,0 +1,15 @@
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+class Solution {
+
+    public double solution(int[] numbers) {
+        double sum = 0;
+        int len = numbers.length;
+
+        for (int i = 0; i < len; i++) {
+            sum = sum + numbers[i];
+
+        }
+        return sum / len;
+    }
+}
