4593fb75fa78542a570b13e88d7668411e7ee87c
park seong ho <hgg0305@daum.net>
2025-01-07 09:00:43 +0900

    문제 22

---
+++ problem22.java
@@ -0,0 +1,27 @@
+//두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+//예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+
+public class problem22 {
+    public long solution(int a, int b) {
+        int small = 0;
+         int big =0;
+         if (a<b){
+             small = a;
+             big = b;
+         }
+         else {
+             small = b;
+             big = a;
+         }
+         int sum =0;
+       for(int i = small; i <= big; i++)
+       {
+           sum = sum + i;
+       }
+         
+         
+         return sum;
+     }
+    
+}
