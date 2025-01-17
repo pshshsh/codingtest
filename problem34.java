cf6285fb732458b14aabfddedfca61f739bd88f3
park seong ho <hgg0305@daum.net>
2025-01-17 09:10:42 +0900

    문제 34

---
+++ problem34.java
@@ -0,0 +1,17 @@
+/* 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다. */
+
+import java.util.Arrays;
+
+class problem34 {
+    public String solution(String s) {
+        char[] arr = s.toCharArray(); // 문자 배열로
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}
